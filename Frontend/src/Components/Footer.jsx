083cea5aea125e7e8511b47adcf9cd56a83aa5b9
imAbhay22 <choudharyabhay555@gmail.com>
2025-05-22 12:48:28 +0530

    updated nav and footer

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -4,19 +4,20 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <>
-      <div className="text-xl text-center text-white bg-[#1E1F2F] h-[30vh] flex flex-col justify-center items-center gap-3">
-        <div>
+    <footer className="bg-[#1E1F2F] text-white py-10 px-4">
+      <div className="flex flex-col items-center max-w-4xl gap-6 mx-auto text-center">
+        <p className="text-xl sm:text-2xl">
           <span className="text-3xl font-semibold">Hello,</span> we can become
           friends if you
-        </div>
-        <div className="flex items-center gap-4">
+        </p>
+
+        <div className="flex flex-col items-center justify-center gap-4 sm:flex-row">
           {/* My Instagram */}
           <a
             href="https://www.instagram.com/im._Abhay_"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-1 text-pink-500 transition-colors hover:text-pink-400"
+            className="flex items-center gap-2 text-pink-500 transition-colors hover:text-pink-400"
           >
             <FaInstagram size={24} />
             My Instagram
@@ -27,15 +28,15 @@ const Footer = () => {
             href="https://x.com/intent/follow?original_referer=https%3A%2F%2Finspirobot.me%2F&screen_name=TheInspiroBot"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center gap-1 text-blue-400 transition-colors hover:text-blue-300"
+            className="flex items-center gap-2 text-blue-400 transition-colors hover:text-blue-300"
           >
-            Api Owner :
+            Api Owner:
             <FaXTwitter size={24} />
             TheInspiroBot
           </a>
         </div>
       </div>
-    </>
+    </footer>
   );
 };
 
